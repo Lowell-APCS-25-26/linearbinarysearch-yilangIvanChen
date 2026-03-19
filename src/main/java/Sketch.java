339fefbfb38063fbd7829e621038dccf8a607645
yilangIvanChen <yichen5@s.sfusd.edu>
2026-03-19 09:36:34 -0700

    Implement search methods in Sketch.java

--- src/main/java/Sketch.java
+++ src/main/java/Sketch.java
@@ -24,20 +24,46 @@ public class Sketch {
     new Item(19967, 45)
   };                             
   public int linearSearch(int catNumToFind){
-    //complete this method
+    for (int i = 0; i < store.length; i++){
+      if (catNumToFind == i.getCatNum())
+        return i;
+    }
     return -1;
   }
+
   public int recursiveLinearSearch(int catNumToFind, int startIndex){
-    //complete this method
-    return -1;
+    if (startIndex >= store.length)
+      return -1;
+    else if (store[startIndex].getCatNum == catNumToFind)
+      return startIndex;
+    else
+      return recursiveLinearSearch(catNumToFind, startIndex+1)
   }
   public int binarySearch(int catNumToFind){
-    //complete this method    
+    int low = 0;
+    int high = store.length-1;
+    int guess;
+    while (low <= high){
+      guess = (low+high)/2;
+      if (store[guess].getCatNum() == catNumToFind)
+        return guess;
+      else if (store[guess].getCatNum() > catNumToFind)
+        high = guess-1;
+      else
+        low = guess+1;
+    }
     return -1;
   }
   public int recursiveBinarySearch(int catNumToFind, int nLow, int nHigh){
-    //complete this method    
-    return -1;
+    int guess = (nLow+nHigh)/2;
+    if (store[guess].getCatNum() == catNumToFind)
+      return guess;
+    else if (low > high)
+      return -1;
+    else if (store[guess].getCatNum() > catNumToFind)
+      return recursiveBinarySearch(catNumToFind, nLow, guess-1);
+    else
+      return recursiveBinarySearch(catNumToFind, guess+1, nHigh);
   }
   public void tester(){
     int[] tests = {0, 183, 184, 2370, 15320, 19967, 19968};
